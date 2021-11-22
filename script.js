5935f7eca7855c2c247dc1fcfbb6fc46012bb701
SergioEvando23 <sergioevandocosta@gmail.com>
2021-11-22 19:40:41 -0300

    adicionada cor preta como primeira cor da paleta de cores.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.color')[0].style.backgroundColor = '#836FFF';
+document.querySelectorAll('.color')[0].style.backgroundColor = '#000000';
 document.querySelectorAll('.color')[1].style.backgroundColor = '#7FFFD4';
 document.querySelectorAll('.color')[2].style.backgroundColor = '#DAA520';
 document.querySelectorAll('.color')[3].style.backgroundColor = '#FF00FF';
