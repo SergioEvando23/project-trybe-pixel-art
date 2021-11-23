eea5dc13b66284c0fa8d80e0d28a7cb7ef9c390d
SergioEvando23 <sergioevandocosta@gmail.com>
2021-11-22 22:20:57 -0300

    adicionada cor preta ao primeiro quadro da paleta de cores

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ document.querySelectorAll('.color')[0].style.backgroundColor = '#000000';
 document.querySelectorAll('.color')[1].style.backgroundColor = '#7FFFD4';
 document.querySelectorAll('.color')[2].style.backgroundColor = '#DAA520';
 document.querySelectorAll('.color')[3].style.backgroundColor = '#FF00FF';
+
+let pixelLine = document.createElement('section');
+
+for (let i1 = 0; i1 <= 5; i1 += 1) {
+    pixelLine.id = 'pixel-line';    
+    document.getElementById('pixel-board').appendChild(pixelLine);
+    
+    for (let i2 = 0; i2 <= 5; i2 += 1) {
+        let divPixel = document.createElement('div');
+        divPixel.className = 'color';
+        document.getElementById('pixel-line').appendChild(divPixel);
+    }
+}
+
