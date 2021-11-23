b35f460fc64037b8cb0cc0cbda7346d31cb70727
SergioEvando23 <sergioevandocosta@gmail.com>
2021-11-23 00:45:11 -0300

    quadro de pixels inserido

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 for ( let i = 0; i < 4; i += 1) {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.className = 'color';
     div.class = 'color';
     document.getElementById('color-palette').appendChild(div);
@@ -11,14 +11,16 @@ document.querySelectorAll('.color')[2].style.backgroundColor = '#DAA520';
 document.querySelectorAll('.color')[3].style.backgroundColor = '#FF00FF';
 
 for (let i1 = 0; i1 < 5; i1 += 1) {
-    let pixelLine = document.createElement('section');
+    const pixelLine = document.createElement('section');
     pixelLine.id = 'pixel-line';
     document.getElementById('pixel-board').appendChild(pixelLine);
     for (let i2 = 0; i2 < 5; i2 += 1) {
-        let divPixel = document.createElement('div');
+        const divPixel = document.createElement('div');
         divPixel.className = 'pixel';
         divPixel.style.backgroundColor = '#FFFFFF';
         document.querySelectorAll('#pixel-line')[i1].appendChild(divPixel);
-    }
+    };
 }
 
+const selectedColor = document.querySelectorAll('.color')[0].style.backgroundColor;
+selectedColor.className = 'selected';
