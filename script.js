833e56d0f57cedfd3fc6ad5d92021a239379fc88
SergioEvando23 <sergioevandocosta@gmail.com>
2021-11-22 23:31:07 -0300

    completando paleta de cores requisito 2

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+for ( let i = 0; i < 4; i += 1) {
+    let div = document.createElement('div');
+    div.className = 'color';
+    div.class = 'color';
+    document.getElementById('color-palette').appendChild(div);
+}
+
 document.querySelectorAll('.color')[0].style.backgroundColor = '#000000';
 document.querySelectorAll('.color')[1].style.backgroundColor = '#7FFFD4';
 document.querySelectorAll('.color')[2].style.backgroundColor = '#DAA520';
 document.querySelectorAll('.color')[3].style.backgroundColor = '#FF00FF';
 
-let pixelLine = document.createElement('section');
-
-for (let i1 = 0; i1 <= 5; i1 += 1) {
-    pixelLine.id = 'pixel-line';    
+for (let i1 = 0; i1 < 5; i1 += 1) {
+    let pixelLine = document.createElement('section');
+    pixelLine.id = 'pixel-line';
     document.getElementById('pixel-board').appendChild(pixelLine);
-    
-    for (let i2 = 0; i2 <= 5; i2 += 1) {
+    for (let i2 = 0; i2 < 5; i2 += 1) {
         let divPixel = document.createElement('div');
-        divPixel.className = 'color';
-        document.getElementById('pixel-line').appendChild(divPixel);
+        divPixel.className = 'pixel';
+        divPixel.style.backgroundColor = '#FFFFFF';
+        document.querySelectorAll('#pixel-line')[i1].appendChild(divPixel);
     }
 }
 
